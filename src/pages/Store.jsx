c157deca4a592a7ea246313be51e4e7a0b2143c9
Vivek Kumar <ui.vivekk@gmail.com>
2024-01-23 12:13:37 +0530

    Implemented route protection

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import { Container, Row } from 'react-bootstrap'
-import CartItem from '../components/Cart/CartItem'
-import { useCartContext } from '../context/cart-context'
-import ProductList from '../components/Product/ProductList'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import CartItem from "../components/Cart/CartItem";
+import { useCartContext } from "../context/cart-context";
+import { useAuthContext } from "../context/auth-context";
+import ProductList from "../components/Product/ProductList";
+import { Redirect } from "react-router-dom";
 
 const Store = () => {
-	const {IsCartOpen} = useCartContext()
-  return (
-    <>
-     <Container>
-        <Row>
-          <ProductList />
-        </Row>
-      </Container>
-      {IsCartOpen && <CartItem />}
-    </>
-  )
-}
+	const { IsCartOpen } = useCartContext();
+	const { IsLoggedIn } = useAuthContext();
+  if (!IsLoggedIn) {
+    return <Redirect to="/login" />;
+  }
+	return (
+		<>
+			<Container>
+				<Row>
+					<ProductList />
+				</Row>
+			</Container>
+			{IsCartOpen && <CartItem />}
+		</>
+	);
+};
 
-export default Store
+export default Store;
